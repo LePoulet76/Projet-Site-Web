8748535a75c39c31855513ec4b0c9820a8e2ba51
Arthur <poulain.arthur@orange.fr>
2025-12-18 19:10:57 +0100

    Uniformisation de la mise en commun

--- frontend/src/pages/InGame.js
+++ frontend/src/pages/InGame.js
@@ -2,15 +2,15 @@ import React, { useEffect, useState } from "react";
 import Waveform from "../components/Waveform";
 import Tchat from "../components/Tchat";
 import { socket } from "../socket";
-import Scoreboard from "../components/ScoreBoard";
-import Player from  ".../backend/classes/Player";
+/*import Scoreboard from "../components/ScoreBoard";*/
+/*import Player from  ".../backend/classes/Player";*/
 
 export default function GamepPage() {
   
   return (
     <div className="p-6 max-w-md mx-auto">
       <Tchat socket={socket} />
-      <Scoreboard players={players} />
+      {/*<Scoreboard players={players} />*/}
       <Waveform audioUrl="/content/musics/anime/SAO.mp3" /> 
     </div>
   );
