4e915e0f21e788b982f644a19ff6ec5a1fb65c23
Arthur <poulain.arthur@orange.fr>
2025-12-19 01:44:02 +0100

    fin des correctif

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,6 @@ import InGame from "./pages/InGame";
 import Inscription from "./pages/inscription";
 import Login from "./pages/login";
 import lobbyCreation from "./pages/lobbyCreation";
-import hub from "./pages/hub";
 import { InfoJoueurProvider } from "./pages/InfoJoueurContext";
 
 // import Footer from "./components/Footer";
@@ -20,7 +19,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/lobbyCreation" element={<lobbyCreation />} />
-          <Route path="/hub" element={<hub />} />
           <Route path="/ingame" element={<InGame />} />
           <Route path="/inscription" element={<Inscription />} />
           <Route path="/login" element={<Login />} />
